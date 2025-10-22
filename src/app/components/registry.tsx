fad833ee76e5d8f433bb35b86d44ef07a9c0f41c
Tyler Rivers <me@tylerrivers.com>
2025-10-22 18:24:23 -0500

    :bug: Fix apostrophe

--- src/app/components/registry.tsx
+++ src/app/components/registry.tsx
@@ -2,7 +2,7 @@ const RegistrySection = () => (
   <section id="registry" className="text-center">
     <h2 className="text-3xl font-bold mb-6 font-horley text-[#f2df93ff]">Wedding Registries</h2>
     <p className="max-w-2xl mx-auto mb-8">
-      Your presence at our wedding is the greatest gift of all. However, should you wish to honor us with a gift, we are registered on Zola. If you'd like, you can also contribute to our honeymoon through Venmo, which we would also appreciate.
+      Your presence at our wedding is the greatest gift of all. However, should you wish to honor us with a gift, we are registered on Zola. If you&apos;d like, you can also contribute to our honeymoon through Venmo, which we would also appreciate.
     </p>
     <div className="flex flex-wrap justify-center gap-4">
       {['Zola', 'Venmo'].map((store, index) => (
