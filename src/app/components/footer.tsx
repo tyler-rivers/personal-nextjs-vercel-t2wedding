343f537de18cf41c6a3d5780dbdf4f75ff5fed6c
Tyler Rivers <me@tylerrivers.com>
2026-06-14 10:15:24 -0500

    Update registry links to be Venmo only

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,6 +1,6 @@
 const FooterSection = () => (
   <footer className="bg-gray-900 text-[#f7fbfc] text-center p-6 mt-16">
-    <p className="text-sm">© 2025 Taylor & Tyler Rivers. All Rights Reserved.</p>
+    <p className="text-sm">© 2026 Taylor & Tyler Rivers. All Rights Reserved.</p>
   </footer>
 );
 
--- src/app/components/registry.tsx
+++ src/app/components/registry.tsx
@@ -1,11 +1,7 @@
 const registries = [
   {
-    label: 'Zola',
-    url: 'https://www.zola.com/registry/phillipsriverswedding'
-  },
-  {
     label: 'Venmo',
-    url: '#'
+    url: 'https://account.venmo.com/u/Tyler-A-Rivers'
   }
 ]
 
@@ -13,13 +9,13 @@ const RegistrySection = () => (
   <section id="registry" className="text-center">
     <h2 className="text-3xl font-bold mb-6 font-horley text-[#f2df93ff]">Wedding Registries</h2>
     <p className="max-w-2xl mx-auto mb-8">
-      Your presence at our wedding is the greatest gift of all. However, should you wish to honor us with a gift, we would prefer a donation to our Honeymoon fund - we are fortunate enough to have all the sheets and stand mixers and other housewares that swe can use.
+      Your presence at our wedding is the greatest gift of all. However, should you wish to honor us with a gift, we would prefer a donation to our Honeymoon fund - we are fortunate enough to have all the bed sheets and stand mixers and other housewares that we can use.
     </p>
     <div className="flex flex-wrap justify-center gap-4">
       {registries.map((store, index) => (
         <a
           key={index}
-          href={store.url} // Replace with actual registry links
+          href={store.url}
           className="bg-indigo-600 text-white font-semibold py-3 px-8 rounded-full shadow-lg hover:bg-indigo-700 transition duration-300"
           target="_blank"
         >
